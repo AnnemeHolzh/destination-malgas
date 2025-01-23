a7d0bd09936dbedfef6c251b0b3001896de789a0
Try-Angle1134 <Jaspervann1313@gmail.com>
2025-01-23 23:27:34 +0200

    Implement dummy data creation functions in Home component; update data models to export interfaces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-"use client"
 import Image from "next/image";
 import { createHouse, generateHouseId } from "./services/houseService";
 import { app } from "./Firebase/firebaseConfig";
 import { createUser, generateUserId } from "./services/userService";
 import { createAmenity, generateAmenityId } from "./services/amenityService";
+import { HeroCarouselWrapper } from "./components/client-wrappers/hero-carousel-wrapper";
+import { AboutSection } from "./components/about-section";
+import { ReasonsCarousel } from "./components/reasons-carousel";
 
 export default function Home() {
   return (
