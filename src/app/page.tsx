a0d91af019660dfea2ba5014b9574badcca53815
Try-Angle1134 <Jaspervann1313@gmail.com>
2025-01-29 22:04:15 +0200

    Implement authentication features: add login modal, user authentication service, and update navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-'use client';
-
-import Image from "next/image";
-import { HeroCarouselWrapper } from "./components/client-wrappers/hero-carousel-wrapper";
-import { AboutSection } from "./components/about-section";
-import { ReasonsCarousel } from "./components/reasons-carousel";
+"use client"
+import { HeroCarouselWrapper } from "./components/client-wrappers/hero-carousel-wrapper"
+import { AboutSection } from "./components/about-section"
+import { ReasonsCarousel } from "./components/reasons-carousel"
 
 export default function Home() {
   return (
